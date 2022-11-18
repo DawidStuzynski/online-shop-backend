9f0db78b4cd37c2fd414533cd486ed111abe85d0
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2022-11-18 07:07:52 +0100

    initial commit

---
+++ src/main/java/com/example/onlineshopbackend/OnlineShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShopBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlineshopbackend/OnlineShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
