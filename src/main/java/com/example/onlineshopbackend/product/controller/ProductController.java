644b3c6431196fcff0d4614e96f098684ca73bbc
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2022-11-19 23:23:52 +0100

    feat: Add pagination to fetching products

--- src/main/java/com/example/onlineshopbackend/product/controller/ProductController.java
+++ src/main/java/com/example/onlineshopbackend/product/controller/ProductController.java
@@ -3,13 +3,10 @@ package com.example.onlineshopbackend.product.controller;
 import com.example.onlineshopbackend.product.model.Product;
 import com.example.onlineshopbackend.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.print.PrintService;
-import java.math.BigDecimal;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +15,7 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/products")
-    public List<?> getProducts() {
-        return   productService.getProducts();
+    public Page<Product> getProducts(Pageable pageable) {
+        return productService.getProducts(pageable);
     }
 }
--- src/main/java/com/example/onlineshopbackend/product/service/ProductService.java
+++ src/main/java/com/example/onlineshopbackend/product/service/ProductService.java
@@ -3,9 +3,9 @@ package com.example.onlineshopbackend.product.service;
 import com.example.onlineshopbackend.product.model.Product;
 import com.example.onlineshopbackend.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,8 +13,8 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public List<Product> getProducts() {
-        return productRepository.findAll();
+    public Page<Product> getProducts(Pageable pageable) {
+        return productRepository.findAll(pageable);
     }
 
 }
