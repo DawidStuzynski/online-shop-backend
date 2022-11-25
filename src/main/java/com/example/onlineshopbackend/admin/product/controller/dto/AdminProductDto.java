fac0b6b8db1ddbe4e8a5c30bf52ec667676ebf2a
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2022-11-25 23:46:07 +0100

    refactor: Remove entity annotation from AdminProductDto

--- src/main/java/com/example/onlineshopbackend/admin/product/controller/dto/AdminProductDto.java
+++ src/main/java/com/example/onlineshopbackend/admin/product/controller/dto/AdminProductDto.java
@@ -1,23 +1,12 @@
 package com.example.onlineshopbackend.admin.product.controller.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.math.BigDecimal;
 
-@Entity
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor
 public class AdminProductDto {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
     private String description;
