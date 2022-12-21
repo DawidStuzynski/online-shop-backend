11e9dd20dd75bb4b6bd2f0795eff3dc3551bc058
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2022-12-21 16:53:13 +0100

    feat: Add validation for slug

--- src/main/java/com/example/onlineshopbackend/product/controller/ProductController.java
+++ src/main/java/com/example/onlineshopbackend/product/controller/ProductController.java
@@ -3,10 +3,14 @@ package com.example.onlineshopbackend.product.controller;
 import com.example.onlineshopbackend.product.model.Product;
 import com.example.onlineshopbackend.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Pattern;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,5 +21,14 @@ public class ProductController {
     @GetMapping("/products")
     public Page<Product> getProducts(Pageable pageable) {
         return productService.getProducts(pageable);
+    }
+
+    @GetMapping("/products/{slug}")
+    public Product getProduct(
+            @PathVariable
+            @Pattern(regexp = "[a-z0-9\\-]+")
+            @Length(max = 255)
+            String slug) {
+        return productService.getProduct(slug);
     }
 }
