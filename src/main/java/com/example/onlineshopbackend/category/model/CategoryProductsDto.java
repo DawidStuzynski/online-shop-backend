75f6cdc8fbca54c5b00f859a1fac09c7e80bd3ec
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-01-26 19:19:12 +0100

    feat: Implement returning review dto calling by category

--- src/main/java/com/example/onlineshopbackend/category/model/CategoryProductsDto.java
+++ src/main/java/com/example/onlineshopbackend/category/model/CategoryProductsDto.java
@@ -1,7 +1,8 @@
 package com.example.onlineshopbackend.category.model;
 
+import com.example.onlineshopbackend.product.controller.dto.ProductListDto;
 import com.example.onlineshopbackend.product.model.Product;
 import org.springframework.data.domain.Page;
 
-public record CategoryProductsDto(Category category, Page<Product> products) {
+public record CategoryProductsDto(Category category, Page<ProductListDto> products) {
 }
--- src/main/java/com/example/onlineshopbackend/category/service/CategoryService.java
+++ src/main/java/com/example/onlineshopbackend/category/service/CategoryService.java
@@ -3,10 +3,12 @@ package com.example.onlineshopbackend.category.service;
 import com.example.onlineshopbackend.category.model.Category;
 import com.example.onlineshopbackend.category.model.CategoryProductsDto;
 import com.example.onlineshopbackend.category.repository.CategoryRepository;
+import com.example.onlineshopbackend.product.controller.dto.ProductListDto;
 import com.example.onlineshopbackend.product.model.Product;
 import com.example.onlineshopbackend.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,6 +30,19 @@ public class CategoryService {
     public CategoryProductsDto getCategoriesWithProducts(String slug, Pageable pageable) {
         Category category = categoryRepository.findBySlug(slug);
         Page<Product> page = productRepository.findByCategoryId(category.getId(), pageable);
-        return new CategoryProductsDto(category, page);
+
+        List<ProductListDto> productListDtos = page.getContent().stream()
+                .map(product -> ProductListDto.builder()
+                        .id(product.getId())
+                        .name(product.getName())
+                        .description(product.getDescription())
+                        .price(product.getPrice())
+                        .currency(product.getCurrency())
+                        .image(product.getImage())
+                        .slug(product.getSlug())
+                        .build())
+                .toList();
+
+        return new CategoryProductsDto(category, new PageImpl<>(productListDtos, pageable, page.getTotalElements()));
     }
 }
