057b59611ab2b6e9251cf4747a2890a08bc88995
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-02-02 18:18:29 +0100

    feat: Improve summing cart

--- src/main/java/com/example/onlineshopbackend/cart/controller/mapper/CartMapper.java
+++ src/main/java/com/example/onlineshopbackend/cart/controller/mapper/CartMapper.java
@@ -65,6 +65,6 @@ public class CartMapper {
         return items.stream()
                 .map(CartMapper::calculateLineValue)
                 .reduce(BigDecimal::add)
-                .orElseThrow();
+                .orElse(BigDecimal.ZERO);
     }
 }
