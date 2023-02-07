001a497051846085fe082405e302de5ee3cf4a87
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-02-07 17:22:42 +0100

    feat: Add placeOrder method in controller

--- src/main/java/com/example/onlineshopbackend/order/controller/OrderController.java
+++ src/main/java/com/example/onlineshopbackend/order/controller/OrderController.java
@@ -1,7 +1,11 @@
 package com.example.onlineshopbackend.order.controller;
 
+import com.example.onlineshopbackend.order.model.dto.OrderDto;
+import com.example.onlineshopbackend.order.model.dto.OrderSummary;
 import com.example.onlineshopbackend.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     private final OrderService orderService;
+
+    @PostMapping
+    public OrderSummary placeOrder(@RequestBody OrderDto orderDto) {
+        return orderService.placeOrder(orderDto);
+    }
+
 }
