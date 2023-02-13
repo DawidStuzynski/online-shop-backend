db9fe0c5a9eeeb3192dfc9d12084ec97bfdc3218
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-02-13 19:33:51 +0100

    refactor: Improve request mapping in OrderController

--- src/main/java/com/example/onlineshopbackend/order/controller/OrderController.java
+++ src/main/java/com/example/onlineshopbackend/order/controller/OrderController.java
@@ -9,10 +9,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/orders")
 public class OrderController {
 
     private final OrderService orderService;
@@ -24,7 +26,7 @@ public class OrderController {
         return orderService.placeOrder(orderDto);
     }
 
-    @GetMapping
+    @GetMapping("/initData")
     public InitOrder initData() {
         return InitOrder.builder()
                 .shipment(shipmentService.getShipments())
